a24cd28d3ba1383e4b5c93440ac75c929d3ba021
Denis Tuzhik <dentuzhik@gmail.com>
2016-03-26 17:03:17 +0300

    Test automatical deployment to AWS

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 exports.handler = function(event, context) {
     console.log('Hello World! Time remaining: ', context.getRemainingTimeInMillis());
+    console.log('This is a sample message to test deployment for Julia!');
 };
