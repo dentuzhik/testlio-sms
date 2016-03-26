bba476bb37a87d5e4f3436badff804662a17a1d6
Denis Tuzhik <dentuzhik@gmail.com>
2016-03-26 20:42:48 +0300

    Add ability to kill handler from external SNS event

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+var _ = require('lodash');
+
+function tryToKillHandler(event, context) {
+    var KILL_MARKER = 'KILL';
+
+    if (_.get(event, 'Records.0.Sns.Message') === KILL_MARKER) {
+        context.done(null, {
+            message: 'Successfully killed handler: ' + context.invokedFunctionArn
+        });
+    }
+}
+
 exports.handler = function(event, context) {
-    console.log(event, context.getRemainingTimeInMillis());
+    // Check if Lambda execution should be killed
+    // This is required so polling of SQS won't go forever and consume all the Quota
+    tryToKillHandler(event, context);
 };
