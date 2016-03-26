3085c891cc7167b4aefb86b1e6042f5a2f16394e
Denis Tuzhik <dentuzhik@gmail.com>
2016-03-26 19:56:59 +0300

    Test events from SNS

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
 exports.handler = function(event, context) {
-    console.log('Hello World! Time remaining: ', context.getRemainingTimeInMillis());
-    console.log('This is a sample message to test deployment for Julia!');
+    console.log(event, context.getRemainingTimeInMillis());
 };
